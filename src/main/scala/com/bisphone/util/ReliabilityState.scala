e51481859edde51cd1c555a49f3190518f6ffa21
Reza Samei <reza.samei.g@gmail.com>
2018-05-06 11:41:40 +0430

    Fix Typo

--- src/main/scala/com/bisphone/util/ReliabilityState.scala
+++ src/main/scala/com/bisphone/util/ReliabilityState.scala
@@ -1,43 +1,34 @@
 package com.bisphone.util
 
+import com.bisphone.util.ReliabilityState.{ Green, Red, Shutdown, Yellow }
 
-sealed trait ReliabilityState { def from: Long }
+
+sealed trait ReliabilityState {
+    def from: Long
+    def toRed(time: Long = System.currentTimeMillis()): ReliabilityState = Red(time, this)
+    def toYellow(time: Long = System.currentTimeMillis()): ReliabilityState  = Yellow(time, this)
+    def toGreen(time: Long = System.currentTimeMillis()) : ReliabilityState = Green(time, this)
+    def shtudown(time: Long = System.currentTimeMillis()): ReliabilityState = Shutdown(time, this)
+}
 
 object ReliabilityState {
 
-    def apply(time: Long): ReliabilityState = Bootstrap(time)
+    def apply(time: Long = System.currentTimeMillis()): ReliabilityState = Bootstrap(time)
 
-    final case class Bootstrap private[ReliabilityState] (from: Long) extends ReliabilityState {
-        def toRed(time: Long = System.currentTimeMillis()): ReliabilityState = Red(time, this)
-        def toYellow(time: Long = System.currentTimeMillis()): ReliabilityState  = Yellow(time, this)
-        def toGreen(time: Long = System.currentTimeMillis()) : ReliabilityState = Green(time, this)
-        def shtudown(time: Long = System.currentTimeMillis()): ReliabilityState = Shutdown(time, this)
-    }
+    final case class Bootstrap private[ReliabilityState] (from: Long) extends ReliabilityState
 
     final case class Red[T <: ReliabilityState] private[ReliabilityState](
         from: Long, prev: T
-    ) extends ReliabilityState {
-        def toYellow(time: Long = System.currentTimeMillis()): ReliabilityState  = Yellow(time, this)
-        def toGreen(time: Long = System.currentTimeMillis()) : ReliabilityState = Green(time, this)
-        def shtudown(time: Long = System.currentTimeMillis()): ReliabilityState = Shutdown(time, this)
-    }
+    ) extends ReliabilityState
 
 
     final case class Yellow[T <: ReliabilityState] private[ReliabilityState](
         from: Long, prev: T
-    ) extends ReliabilityState {
-        def toRed(time: Long = System.currentTimeMillis()): ReliabilityState = Red(time, this)
-        def toGreen(time: Long = System.currentTimeMillis()) : ReliabilityState = Green(time, this)
-        def shtudown(time: Long = System.currentTimeMillis()): ReliabilityState = Shutdown(time, this)
-    }
+    ) extends ReliabilityState
 
     final case class Green[T <: ReliabilityState] private[ReliabilityState] (
         from: Long, prev: T
-    ) extends ReliabilityState {
-        def toRed(time: Long = System.currentTimeMillis()): ReliabilityState = Red(time, this)
-        def toYellow(time: Long = System.currentTimeMillis()): ReliabilityState  = Yellow(time, this)
-        def shtudown(time: Long = System.currentTimeMillis()): ReliabilityState = Shutdown(time, this)
-    }
+    ) extends ReliabilityState
 
     final case class Shutdown[T <: ReliabilityState] private[ReliabilityState](
         from: Long, prev: T
